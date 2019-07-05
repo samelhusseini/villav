473b7078aa6d6326bd334372d06b860abb949b1b
Sam El-Husseini <samelh@microsoft.com>
2019-07-05 09:50:18 -0700

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+      <section className="hero is-fullheight-with-navbar is-danger is-bold">
+          <div className="hero-body">
+              <div className="container has-text-centered">
+                  <h1 className="title">
+                      NOT FOUND</h1>
+                  <h2 className="subtitle">
+                  You just hit a route that doesn&#39;t exist... the sadness. </h2>
+
+                  <Link to="/" className="button is-primary">Back to Home</Link>
+              </div>
+          </div>
+      </section>
   </Layout>
 )
 

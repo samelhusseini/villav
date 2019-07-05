928305b74a3f902cbd657a6cb011c71a3934deb7
Sam El-Husseini <samelh@microsoft.com>
2019-07-04 22:35:54 -0700

    Remove http content

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -15,18 +15,6 @@ export default class Gallery extends React.Component {
 
     render() {
         const images = [
-            {
-                original: '/img/main.jpg',
-                thumbnail: 'http://lorempixel.com/250/150/nature/1/',
-            },
-            {
-                original: 'http://lorempixel.com/1000/600/nature/2/',
-                thumbnail: 'http://lorempixel.com/250/150/nature/2/'
-            },
-            {
-                original: 'http://lorempixel.com/1000/600/nature/3/',
-                thumbnail: 'http://lorempixel.com/250/150/nature/3/'
-            }
         ]
         return (
             <Layout>

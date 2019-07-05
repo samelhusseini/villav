f25e8cbda946d686069ec2101efbc592af2afdb0
Sam El-Husseini <samelh@microsoft.com>
2019-07-05 09:53:08 -0700

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,8 +11,8 @@ const NotFoundPage = () => (
                       NOT FOUND</h1>
                   <h2 className="subtitle">
                   You just hit a route that doesn&#39;t exist... the sadness. </h2>
-
-                  <Link to="/" className="button is-primary">Back to Home</Link>
+                  <br />
+                  <Link to="/" className="button">Back to Home</Link>
               </div>
           </div>
       </section>
